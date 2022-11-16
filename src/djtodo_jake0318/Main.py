fcad0454eca3bd0a6fa6b71c0f95cce8482ff4bc
dongjin2008 <dkim@icsparis.fr>
2022-11-16 17:00:24 +0100

    some files added

--- src/djtodo_jake0318/Main.py
+++ src/djtodo_jake0318/Main.py
@@ -1,58 +1,27 @@
-import os
-from pathlib import Path
-import getpass
+import sys
+import File
+import json
+from File import file
+import pickle
 
-username = getpass.getuser()
-base = Path("/home/{}/.config/".format(username))
-folder = Path("{}/djtodo".format(base))
-file = Path("{}/djtodo/task.txt".format(base))
+File.create_file()
+argument = ""
+try:
+    argument = sys.argv[1]
+except IndexError:
+    pass
+test= {}
+data = {}
 
-if not folder.is_dir():
-        os.mkdir(folder)
-if not file.is_file(): 
-        f = open(file, "w+")
-
-
-print("""
-1. Create new tasks
-2. List tasks
-3. Quit
-""")
-
-# aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa   2022/03/18  [x]                 
-
-
-UI = input("Enter the number: ")
-
-
-if UI == "1":
-    f = open(file, "a")
-    while True:
-        task = input("Task: ")
-        date = input("Due Date(yyyy/mm/dd): ")
-        did = False
-        f.write("{} {} {}\n".format(task, date, did))
-        cnt = input("Create more? [y/N]")
-        if cnt.lower() == "y":
-            continue
-        if cnt.lower() == "n" or cnt == "":
-            break
-
-if UI == "2":
-    f = open(file, "r")
-    x = [line[:-1] for line in f]
-    c = []
-    for d in x:
-        l = d.split(" ")
-        if l[2] == "False":
-            l[2] = "x"
-        else:
-            l[2] = "o"
+i = pickle.load(open("VSF.dat", "rb"))
+print(i)
         
-        c.append("{}\t{}".format(l[0], l[1]))
-        
-
-
-    
-    
+if argument == "add":
+    task = sys.argv[2]
+    due_date = sys.argv[3]
+    data[i] = {'task': task, 'due_date': due_date, 'status': False}
+    i += 1
+    pickle.dump(i, open("VSF.dat", "wb"))
+    with open(File.file, "a") as f:
+        json.dump(data, f)
         
