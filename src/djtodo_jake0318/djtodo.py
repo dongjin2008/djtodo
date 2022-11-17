ac4380dd3a37ecefdbfa4c3d1232dedbef3203b3
dongjin2008 <dkim@icsparis.fr>
2022-11-17 05:48:59 +0100

    cleaned

--- src/djtodo_jake0318/djtodo.py
+++ src/djtodo_jake0318/djtodo.py
@@ -1,8 +1,8 @@
 import sys
 import File
-import json
 from File import file
 import pickle
+import module
 
 File.create_file()
 argument = ""
@@ -10,76 +10,17 @@ try:
     argument = sys.argv[1]
 except IndexError:
     pass
-test= {}
-data = {}
-task_removed = []
-new_data = []
-
-
 i = pickle.load(open("VSF.dat", "rb"))
 
 
-def add(i):
-    try:
-        task = sys.argv[2]
-    except IndexError:
-        print("djtodo: empy argument")
-        sys.exit()
-
-    
-    data = {'name': i,'task': task, 'status': False}
-    
-    i += 1
-    pickle.dump(i, open("VSF.dat", "wb"))
-    with open(File.file, "+r") as f:
-        dt = json.load(f)
-        dt.append(data)
-        f.seek(0)
-        json.dump(dt, f, indent=4)
 if argument == "add":
-    add(i)
-    
-
-
-def ls():
-    with open(File.file, "r", encoding='utf-8') as f:
-        dt = json.load(f)
-        for items in dt:
-            if items["status"] == 0:
-                flag = "x"
-                print("{} {} {}".format(flag, items["name"], items["task"]))
-            else:
-                flag = "o"
-                print("{} {} {}".format(flag, items["name"], items["task"]))
+    module.add(i)
 
 if argument == "list":
-    ls()
+    module.ls()
 
 if argument == "remove":
-    try:
-        name = sys.argv[2]
-    except IndexError:
-        print("djtodo: empy argument")
-        sys.exit()
-    
-    with open(File.file, "r") as f:
-        dt = json.load(f)
+    module.remove(i)
 
-    for items in dt:
-        if items["name"] != int(name):
-            new_data.append(items)
-            index = new_data.index(items)
-            new_data[index]["name"] = index + 1 
-            i = index+2
-            pickle.dump(i, open("VSF.dat", "wb"))
-    
-    with open(File.file, "w") as f:
-        json.dump(new_data, f, indent= 4)
-
-        
-
-        
-
-
-if argument == "toggle":
+if argument == "done":
     pass
